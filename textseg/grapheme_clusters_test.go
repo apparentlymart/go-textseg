add010616cf19150c6a58fd5f8cb37488566eb4e
Martin Atkins <mart@degeneration.co.uk>
2017-05-26 19:40:50 -0700

    Use a ragel-generated scanner rather than a hand-written one
    This still isn't entirely complete, since the grammar lacks rules for
    Hangul syllable sequences, but it's doing a lot better than the old
    scanner was.

--- textseg/grapheme_clusters_test.go
+++ textseg/grapheme_clusters_test.go
@@ -3,7 +3,9 @@ package textseg
 import (
 	"fmt"
 	"reflect"
+	"strings"
 	"testing"
+	"unicode/utf8"
 )
 
 func TestScanGraphemeClusters(t *testing.T) {
@@ -18,9 +20,20 @@ func TestScanGraphemeClusters(t *testing.T) {
 			}
 
 			if !reflect.DeepEqual(got, test.output) {
+				// Also get the rune values resulting from decoding utf8,
+				// since they are generally easier to look up to figure out
+				// what's failing.
+				runes := make([]string, 0, len(test.input))
+				buf := test.input
+				for len(buf) > 0 {
+					r, size := utf8.DecodeRune(buf)
+					runes = append(runes, fmt.Sprintf("0x%04x", r))
+					buf = buf[size:]
+				}
+
 				t.Errorf(
-					"wrong result\ninput: %#v\ngot:   %#v\nwant:  %#v",
-					test.input, got, test.output,
+					"wrong result\ninput: %#v\nrunes: %s\ngot:   %#v\nwant:  %#v",
+					test.input, strings.Join(runes, " "), got, test.output,
 				)
 			}
 		})
