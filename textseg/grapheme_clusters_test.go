e8b86753a12356ab26ddc4549500e765a09b3e2d
Martin Atkins <mart@degeneration.co.uk>
2017-05-27 08:44:09 -0700

    More helpful test result output, with category information

--- textseg/grapheme_clusters_test.go
+++ textseg/grapheme_clusters_test.go
@@ -24,18 +24,43 @@ func TestScanGraphemeClusters(t *testing.T) {
 				// since they are generally easier to look up to figure out
 				// what's failing.
 				runes := make([]string, 0, len(test.input))
+				seqs := make([][]byte, 0, len(test.input))
+				categories := make([]string, 0, len(test.input))
 				buf := test.input
 				for len(buf) > 0 {
 					r, size := utf8.DecodeRune(buf)
 					runes = append(runes, fmt.Sprintf("0x%04x", r))
+					seqs = append(seqs, buf[:size])
+					categories = append(categories, _GraphemeRuneType(r).String())
 					buf = buf[size:]
 				}
 
 				t.Errorf(
-					"wrong result\ninput: %#v\nrunes: %s\ngot:   %#v\nwant:  %#v",
-					test.input, strings.Join(runes, " "), got, test.output,
+					"wrong result\ninput: %s\nutf8s: %s\nrunes: %s\ncats:  %s\ngot:   %s\nwant:  %s",
+					formatBytes(test.input),
+					formatByteRanges(seqs),
+					strings.Join(runes, " "),
+					strings.Join(categories, " "),
+					formatByteRanges(got),
+					formatByteRanges(test.output),
 				)
 			}
 		})
 	}
+}
+
+func formatBytes(buf []byte) string {
+	strs := make([]string, len(buf))
+	for i, b := range buf {
+		strs[i] = fmt.Sprintf("0x%02x", b)
+	}
+	return strings.Join(strs, "   ")
+}
+
+func formatByteRanges(bufs [][]byte) string {
+	strs := make([]string, len(bufs))
+	for i, b := range bufs {
+		strs[i] = formatBytes(b)
+	}
+	return strings.Join(strs, " | ")
 }
