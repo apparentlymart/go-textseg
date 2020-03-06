29d1648e97c6de68a8e9ec67de1f62fae5425001
Martin Atkins <mart@degeneration.co.uk>
2020-03-06 15:15:21 -0800

    autoversion: Wrapper that selects Unicode version automatically
    The main functionality for this repository is Unicode version specific and
    we have a separate module import path for each so that callers can select
    the specific versions they need.
    
    This new "autoversion" module is here to make life a little easier for
    callers who just want to match the text segmentation algorithm/data with
    the Unicode version being used in the Go standard library, so that unicode
    support is consistent throughout the program.
    
    It uses a compile-time switch based on the current Go version to select
    the appropriate textseg major version. It's based on Go version rather
    than Unicode version so that it can be a compile-time rather than a
    runtime decision, or else we'd end up with the tables for all Unicode
    versions linked into every program using this package. Unfortunately this
    means that this package will need maintenance for each new version of Go
    that's released, but hopefully that small annoyance here leads to less
    annoyance for downstreams who can just upgrade this package to gain
    support for new Go/Unicode versions.

---
+++ autoversion/go.mod
@@ -0,0 +1,10 @@
+module github.com/apparentlymart/go-textseg/autoversion
+
+go 1.9
+
+require (
+	github.com/apparentlymart/go-textseg/v10 v10.0.0
+	github.com/apparentlymart/go-textseg/v11 v11.0.0
+	github.com/apparentlymart/go-textseg/v12 v12.0.0
+	github.com/apparentlymart/go-textseg/v9 v9.0.0
+)
---
+++ autoversion/textseg/unicode10.go
@@ -0,0 +1,22 @@
+// +build go1.10,!go1.13
+
+package textseg
+
+import (
+	v10 "github.com/apparentlymart/go-textseg/v10/textseg"
+)
+
+// ScanGraphemeClusters is a split function for bufio.Scanner that splits on
+// grapheme cluster boundaries, using the text segmentation rules from the
+// Unicode version selected by the current Go runtime library.
+//
+// This function will appear to be missing if your current Go version is not
+// supported by your current version of this package.
+func ScanGraphemeClusters(data []byte, atEOF bool) (int, []byte, error) {
+	return v10.ScanGraphemeClusters(data, atEOF)
+}
+
+// UnicodeMajorVersion is the major version of Unicode being used by this
+// package. This should always match the first portion of the string returned
+// by unicode.Version in the Go standard library.
+const UnicodeMajorVersion = 10
