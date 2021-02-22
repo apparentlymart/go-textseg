123b1856967a5b9f3219336239df046cb232d4a8
Martin Atkins <mart@degeneration.co.uk>
2021-02-22 15:33:41 -0800

    autoversion: Update for Go 1.15 and Go 1.16
    Go 1.15 continued using Unicode 12 definitions.
    Go 1.16 introduced Unicode 13 rules.
    
    Once upgraded to latest, callers will now be buildable on both Go 1.15 and
    on Go 1.16, where previously Go 1.14 was the newest supported version.

--- autoversion/go.mod
+++ autoversion/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/apparentlymart/go-textseg/v10 v10.0.0
 	github.com/apparentlymart/go-textseg/v11 v11.0.0
 	github.com/apparentlymart/go-textseg/v12 v12.0.0
+	github.com/apparentlymart/go-textseg/v13 v13.0.0
 	github.com/apparentlymart/go-textseg/v9 v9.0.0
 )
--- autoversion/textseg/unicode13.go
+++ autoversion/textseg/unicode13.go
@@ -1,9 +1,9 @@
-// +build go1.14,!go1.16
+// +build go1.16,!go1.17
 
 package textseg
 
 import (
-	v12 "github.com/apparentlymart/go-textseg/v12/textseg"
+	v13 "github.com/apparentlymart/go-textseg/v13/textseg"
 )
 
 // ScanGraphemeClusters is a split function for bufio.Scanner that splits on
@@ -13,10 +13,10 @@ import (
 // This function will appear to be missing if your current Go version is not
 // supported by your current version of this package.
 func ScanGraphemeClusters(data []byte, atEOF bool) (int, []byte, error) {
-	return v12.ScanGraphemeClusters(data, atEOF)
+	return v13.ScanGraphemeClusters(data, atEOF)
 }
 
 // UnicodeMajorVersion is the major version of Unicode being used by this
 // package. This should always match the first portion of the string returned
 // by unicode.Version in the Go standard library.
-const UnicodeMajorVersion = 12
+const UnicodeMajorVersion = 13
