059ab0bd7a4aa68564738fa09a0c6e71a09ab44a
Cyril Brulebois <cyril@debamax.com>
2022-10-07 06:46:05 +0000

    autoversion: Switch version_agnostic to latest release.

--- autoversion/textseg/version_agnostic.go
+++ autoversion/textseg/version_agnostic.go
@@ -7,7 +7,7 @@ package textseg
 import (
 	"bufio"
 
-	realImpl "github.com/apparentlymart/go-textseg/v12/textseg"
+	realImpl "github.com/apparentlymart/go-textseg/v13/textseg"
 )
 
 // ScanUTF8Sequences is a split function for bufio.Scanner that splits on UTF8 sequence boundaries.
