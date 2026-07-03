7c16b983f0daf537e76ad8267477c8ba14db2a9f
Martin Atkins <mart@degeneration.co.uk>
2026-07-03 11:47:05 -0700

    autoversion: Drop support for Unicode 9 through 13
    These older versions were here for the benefit of older versions of Go, but
    since the Go toolchain now forces us to ratchet this module's minimum
    supported Go version to the largest required version of all of our
    dependencies regardless of conditional compilation this library can no
    longer support Go1.24 and earlier anyway, and so there's no point in
    retaining support for the Unicode versions used by those obsolete Go
    versions.
    
    Although ending support for earlier versions would not have been my choice
    aside from this technical constraint, Go1.25 is currently the earliest
    version of Go that's under security support anyway and so anyone relying
    on earlier versions should either plan to upgrade soon or use earlier
    versions of this autoversion library in the meantime.

--- autoversion/go.mod
+++ autoversion/go.mod
@@ -3,11 +3,6 @@ module github.com/apparentlymart/go-textseg/autoversion
 go 1.25
 
 require (
-	github.com/apparentlymart/go-textseg/v10 v10.0.0
-	github.com/apparentlymart/go-textseg/v11 v11.0.0
-	github.com/apparentlymart/go-textseg/v12 v12.0.0
-	github.com/apparentlymart/go-textseg/v13 v13.0.0
 	github.com/apparentlymart/go-textseg/v15 v15.0.0
 	github.com/apparentlymart/go-textseg/v17 v17.0.0
-	github.com/apparentlymart/go-textseg/v9 v9.0.0
 )
--- autoversion/textseg/version_agnostic.go
+++ autoversion/textseg/version_agnostic.go
@@ -1,8 +1,9 @@
 package textseg
 
 // The functions in this file are not currently specific to a given version of
-// go-textseg and so we just call into the version from the latest upstream
-// major version in all cases.
+// go-textseg, and have actually been removed in later major versions of the
+// main module so this will just continue calling into v15's implementation
+// for now, for backward-compatibility.
 
 import (
 	"bufio"
@@ -13,16 +14,22 @@ import (
 // ScanUTF8Sequences is a split function for bufio.Scanner that splits on UTF8 sequence boundaries.
 //
 // This is included largely for completeness, since this behavior is already built in to Go when ranging over a string.
+//
+// Deprecated: This will be removed in a future version.
 func ScanUTF8Sequences(data []byte, atEOF bool) (int, []byte, error) {
 	return realImpl.ScanUTF8Sequences(data, atEOF)
 }
 
 // TokenCount is a utility that uses a bufio.SplitFunc to count the number of recognized tokens in the given buffer.
+//
+// Deprecated: This will be removed in a future version.
 func TokenCount(buf []byte, splitFunc bufio.SplitFunc) (int, error) {
 	return realImpl.TokenCount(buf, splitFunc)
 }
 
 // AllTokens is a utility that uses a bufio.SplitFunc to produce a slice of all of the recognized tokens in the given buffer.
+//
+// Deprecated: This will be removed in a future version.
 func AllTokens(buf []byte, splitFunc bufio.SplitFunc) ([][]byte, error) {
 	return realImpl.AllTokens(buf, splitFunc)
 }
