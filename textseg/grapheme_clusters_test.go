49e52942d888b8856d14ed686884c75671c21626
Martin Atkins <mart@degeneration.co.uk>
2026-07-02 17:47:59 -0700

    textseg: Test ScanGraphemeClusters with partial input
    The previous test just always provided the entire buffer at once, which
    meant wasn't testing any of the codepaths which need to request more bytes
    before they can answer.
    
    This new test uses a special reader that only produces one byte at a time,
    as a worst-case scenario for e.g. streaming data arriving over a socket
    as a human is typing into an SSH session.

--- textseg/grapheme_clusters_test.go
+++ textseg/grapheme_clusters_test.go
@@ -1,7 +1,9 @@
 package textseg
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"reflect"
 	"strings"
 	"testing"
@@ -14,6 +16,57 @@ func TestScanGraphemeClusters(t *testing.T) {
 	for i, test := range tests {
 		t.Run(fmt.Sprintf("%03d-%x", i, test.input), func(t *testing.T) {
 			got, err := AllTokens(test.input, ScanGraphemeClusters)
+
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if !reflect.DeepEqual(got, test.output) {
+				// Also get the rune values resulting from decoding utf8,
+				// since they are generally easier to look up to figure out
+				// what's failing.
+				runes := make([]string, 0, len(test.input))
+				seqs := make([][]byte, 0, len(test.input))
+				categories := make([]string, 0, len(test.input))
+				buf := test.input
+				for len(buf) > 0 {
+					r, size := utf8.DecodeRune(buf)
+					runes = append(runes, fmt.Sprintf("0x%04x", r))
+					seqs = append(seqs, buf[:size])
+					categories = append(categories, _GraphemeRuneType(r).String())
+					buf = buf[size:]
+				}
+
+				t.Errorf(
+					"wrong result\ninput: %s\nutf8s: %s\nrunes: %s\ncats:  %s\ngot:   %s\nwant:  %s",
+					formatBytes(test.input),
+					formatByteRanges(seqs),
+					strings.Join(runes, " "),
+					strings.Join(categories, " "),
+					formatByteRanges(got),
+					formatByteRanges(test.output),
+				)
+			}
+		})
+	}
+}
+
+// TestScanGraphemeClusters_partial is a variant of TestScanGraphemeClusters
+// that makes sure the same logic works when data arrives in smaller chunks,
+// such as streaming over a socket.
+func TestScanGraphemeClusters_partial(t *testing.T) {
+	tests := unicodeGraphemeTests
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("%03d-%x", i, test.input), func(t *testing.T) {
+			r := &dripReader{test.input}
+			sc := bufio.NewScanner(r)
+			sc.Split(ScanGraphemeClusters)
+			var got [][]byte
+			for sc.Scan() {
+				got = append(got, sc.Bytes())
+			}
+			err := sc.Err()
 
 			if err != nil {
 				t.Fatalf("unexpected error: %s", err)
@@ -63,4 +116,22 @@ func formatByteRanges(bufs [][]byte) string {
 		strs[i] = formatBytes(b)
 	}
 	return strings.Join(strs, " | ")
+}
+
+// dropReader is a reader that returns at most one byte per call, thereby
+// acting as a worst-case scenario for handling streaming data over a socket.
+type dripReader struct {
+	remain []byte
+}
+
+func (r *dripReader) Read(buf []byte) (int, error) {
+	if len(r.remain) == 0 {
+		return 0, io.EOF
+	}
+	if len(buf) == 0 {
+		return 0, nil
+	}
+	buf[0] = r.remain[0]
+	r.remain = r.remain[1:]
+	return 1, nil
 }
